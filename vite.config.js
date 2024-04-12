d4b8dcac9a5f6e8dd2d2c1605841012b97e04e09
Pavan <pavankunchala54@gmail.com>
2024-04-12 15:40:31 +0530

    added deploy.yml file

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/credchainmd-layouts/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/credchainmd-layouts/",
 });
